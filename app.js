6f49e4eec9fefbb1b8a3950ee0811f362d1376c6
Aditya Yadav <adityadav023>
2022-03-29 16:14:17 +0530

    Refactored home route

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const res = require('express/lib/response')
 const app = express()
 
 const urlRouter = require('./routes/url.routes')
@@ -8,8 +7,10 @@ const redirectRouter = require('./routes/redirect.routes')
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send(`hello`)
-})
+    res.status(200).send('<h1>Url Shortner</h1> <a href =/api/v1/redirect> Redirect route </a>')
+}
+    )
+
 app.use('/api/v1/',urlRouter)
 app.use('/api/v1/redirect', redirectRouter)
 
